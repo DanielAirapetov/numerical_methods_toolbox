1b48bf660ca8bf408f86cfbab0a5309a49a2a1f1
Jhon Palaguachi <162334265+Jhon2468@users.noreply.github.com>
2025-12-02 17:21:40 -0500

    Implement root-finding methods with user selection
    Added root-finding methods and user selection for calculations.

--- pages/root_finding.py
+++ pages/root_finding.py
@@ -2,6 +2,16 @@ import streamlit as st
 import sympy as sp
 import numpy as np
 import matplotlib.pyplot as plt
+import os, sys
+
+ROOT = os.path.dirname(os.path.dirname(__file__))
+if ROOT not in sys.path:
+    sys.path.append(ROOT)
+    
+from sections.root_finding.bisection import mark_bisection
+from sections.root_finding.bisection import jhon_bisection
+from sections.root_finding.bisection import franky_bisection
+from sections.root_finding.bisection import daniel_bisection
 
 def user_corrected_equation(equation_str):
     math_functions = ['sin', 'cos', 'tan', 'exp', 'log', 'sqrt', "pow" ]
@@ -31,10 +41,14 @@ def main():
         """,
         unsafe_allow_html=True
     )
+    
     st.markdown('<h1 class="title">Solving Nonlinear Equations</h1>', unsafe_allow_html=True)
+    
     equation = None
     equation = st.text_input("Enter the equation you want to solve: ")
+    
     corrected_equation = None
+    
     if equation != None and equation != "":
         corrected_equation= user_corrected_equation(equation)  
         # Plotting the function
@@ -68,19 +82,38 @@ def main():
 
         st.pyplot(fig)
         plt.clf()
-
-
         
         method = st.selectbox("Select a root-finding method:", ["None", "Bisection Method", "False Position Method", "Secant Method", "Newton Method"])
-        if method != "None":
+        person = st.selectbox("Person:", ["None", "Mark", "Francis", "Daniel", "Jhon"])
+        if method != "None" and person != "None":
             tolerance = st.number_input("Enter the tolerance level:", value = 1 * 10 ** -6, format="%.6f")
             flag = st.selectbox("Select a flag:", [1, 2, 3, 4])      
             if method == "Newton Method":
                 initial_guess = st.number_input("Enter the initial x value:", value=0.0, format="%.4f")
+                
             else:
                 left_bound = st.number_input("Enter the left bound:", value=0.0, format="%.4f")
                 right_bound = st.number_input("Enter the right bound:", value=0.0, format="%.4f")
+                if (person == "Mark" and method == "Bisection Method"):
+                    if st.button("Calculate Root"):
+                        root, iterations = mark_bisection.bisectionMethod(left_bound, right_bound, tolerance, flag, lambda x: evaluate_function(corrected_equation, x))
+                        st.write(f"Root: {root}, Iterations: {iterations}")
+                elif (person == "Jhon" and method == "Bisection Method"):
+                    if st.button("Calculate Root"):
+                        root, iterations = jhon_bisection.bisection(left_bound, right_bound, tolerance, flag, lambda x: evaluate_function(corrected_equation, x))
+                        st.write(f"Root: {root}, Iterations: {iterations}")
+                elif (person == "Francis" and method == "Bisection Method"):
+                    if st.button("Calculate Root"):
+                        root, iterations = franky_bisection.bisection(left_bound, right_bound, tolerance, flag, lambda x: evaluate_function(corrected_equation, x))
+                        st.write(f"Root: {root}, Iterations: {iterations}")
+                else:
+                    if (person == "Daniel" and method == "Bisection Method"):
+                        if st.button("Calculate Root"):
+                            root, iterations = daniel_bisection.bisection(left_bound, right_bound, tolerance, flag, lambda x: evaluate_function(corrected_equation, x))
+                            st.write(f"Root: {root}, Iterations: {iterations}")
     
     
 
 main()
+
+
