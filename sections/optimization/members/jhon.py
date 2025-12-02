06ba23d819bb28346c28dcb5cf2490935a5eb88a
markp561 <235408474+markp561@users.noreply.github.com>
2025-12-02 17:23:37 -0500

    Update jhon.py

--- sections/optimization/members/jhon.py
+++ sections/optimization/members/jhon.py
@@ -1,4 +1,4 @@
-import sympy as sy
+import sympy as sp
 
 def function(data_point, n = 0):
     equation = pow(data_point, 3) - 4 * data_point
@@ -6,17 +6,17 @@ def function(data_point, n = 0):
     if n == 0:
         return equation
     else:
-        x = sy.symbols('x')
+        x = sp.Symbols('x')
         equation = pow(x, 3) - 4 * x
         
         if n == 1:
-            first_derivative = sy.diff(equation, x, 1)
+            first_derivative = sp.diff(equation, x, 1)
             return first_derivative.subs(x, data_point)
         elif n == 2: 
-            second_derivative = sy.diff(equation, x, 2)
+            second_derivative = sp.diff(equation, x, 2)
             return second_derivative.subs(x, data_point)
         
-def newton_min_max(x0, tolerance):
+def newtonMinMax(x0, tolerance):
     a = 1
     i = 1
      
@@ -30,75 +30,70 @@ def newton_min_max(x0, tolerance):
         
     return x_k_1, i
     
-def GoldenSection(a, b, tolerance):
+def goldenSectionMethod(a, b, tolerance, flag, sym_func):
+
+    x = sp.Symbol('x')
+    function = sp.lambdify(x, sym_func, "numpy")
+
     golden_section = 1.618
     
-    a1 = a
-    b1 = b
+    min_max = 0
+    i = 0
     
-    min = None
-    min_i = 0
-        
-    while abs(b1 - a1) > tolerance and min_i <= 1000:
-        x1 = b1 - ((b1 - a1) / golden_section)
-        x2 = a1 + ((b1 - a1) / golden_section)
-        
-        y1 = function(x1)
-        y2 = function(x2)
-        
-        if y1 >= y2:
-            a1 = x1
-            
-            x1 = x2
-            x2 = a1 + ((b1 - a1) / golden_section)
+    if flag == 1:
+
+        while abs(b - a) > tolerance and i <= 1000:
+            x1 = b - ((b - a) / golden_section)
+            x2 = a + ((b - a) / golden_section)
             
             y1 = function(x1)
             y2 = function(x2)
-        else:
-            b1 = x2 
             
-            x2 = x1
-            x1 = b1 - ((b1 - a1) / golden_section)
+            if y1 >= y2:
+                a = x1
+                
+                x1 = x2
+                x2 = a + ((b - a) / golden_section)
+                
+                y1 = function(x1)
+                y2 = function(x2)
+            else:
+                b = x2 
+                
+                x2 = x1
+                x1 = b - ((b - a) / golden_section)
+                
+                y1= function(x1)
+                y2 = function(x2)
             
-            y1= function(x1)
-            y2 = function(x2)
-        
-        min_i += 1
-            
-    min = (a1 + b1) / 2
-    
-    a2 = a
-    b2 = b
-        
-    max = None
-    max_i = 0
-    
-    while abs(b2 - a2) > tolerance and max_i <= 1000:
-        x1 = b2 - ((b2 - a2) / golden_section)
-        x2 = a2 + ((b2 - a2) / golden_section)
-        
-        y1 = function(x1)
-        y2 = function(x2)
-        
-        if y1 <= y2:
-            a2 = x1
-            
-            x1 = x2
-            x2 = a2 + ((b2 - a2) / golden_section)
-            
-            y1 = function(x1)
-            y2 = function(x2)
-        else:
-            b2 = x2
-            
-            x2 = x1
-            x1 = b2 - ((b2 - a2) / golden_section)
-            
-            y1= function(x1)
-            y2 = function(x2)
-            
-        max_i += 1
+            i += 1
+    elif flag == 2:
+        while abs(b - a) > tolerance and i <= 1000:
+                x1 = b - ((b - a) / golden_section)
+                x2 = a + ((b - a) / golden_section)
+                
+                y1 = function(x1)
+                y2 = function(x2)
+                
+                if y1 <= y2:
+                    a = x1
                     
-    max = (a2 + b2) / 2
-    
-    return min, max, min_i, max_i
+                    x1 = x2
+                    x2 = a + ((b - a) / golden_section)
+                    
+                    y1 = function(x1)
+                    y2 = function(x2)
+                else:
+                    b = x2
+                    
+                    x2 = x1
+                    x1 = b - ((b - a) / golden_section)
+                    
+                    y1= function(x1)
+                    y2 = function(x2)
+                    
+                i += 1
+
+    min_max = (a + b) / 2
+        
+    return min_max, i
