aceac85e8d4d7a355440d95d58733dd77443a837
markp561 <235408474+markp561@users.noreply.github.com>
2025-12-02 14:29:47 -0500

    Update francis.py

--- sections/optimization/members/francis.py
+++ sections/optimization/members/francis.py
@@ -2,35 +2,40 @@ import numpy as np
 import sympy as sp
 
 def goldenSectionMethod(a,b, tol, flag, f): # golden section function for nonlinear optimization
+
+    x = sp.Symbol("x")
+    func = sp.lambdify(x, f, "numpy")
+
+
     phi = (1 + np.sqrt(5))/2 # assign phi (golden ratio number)
     error = float('inf') #initialize large error so loop condition is met
     x1 = b - ((b-a)/phi) # initialize x1 based on formula 
     x2 = a + ((b-a)/phi) # initialize x2 as well
     i = 0 # count iterations
     while error >= tol and i < 1000: # loop conditions: if error meets a certain threshold and if theres reasonable number of iterations
+        if flag == 1: #perform GS formula for minimum extrema 
+            if func(x1) >= func(x2): #determine golden sections based on which y value is greater
+                a = x1
+                x1 = x2 
+                x2 = a + ((b-a)/phi)
+            else:
+                b = x2 
+                x2 = x1
+                x1 = b - ((b-a)/phi)
+        
+        elif flag == 2: # GS formula for max extrema, same except based on if fx1 is less than fx2 (opposite of min)
+            if func(x1) <= func(x2):
+                a = x1
+                x1 = x2 
+                x2 = a + ((b-a)/phi)
+            else:
+                b = x2 
+                x2 = x1
+                x1 = b - ((b-a)/phi)
         i += 1 #increment iteration counter in loop
-        if flag == 'min': #perform GS formula for minimum extrema 
-            if f(x1) >= f(x2): #determine golden sections based on which y value is greater
-                a = x1
-                x1 = x2 
-                x2 = a + ((b-a)/phi)
-            else:
-                b = x2 
-                x2 = x1
-                x1 = b - ((b-a)/phi)
-        
-        elif flag == 'max': # GS formula for max extrema, same except based on if fx1 is less than fx2 (opposite of min)
-            if f(x1) <= f(x2):
-                a = x1
-                x1 = x2 
-                x2 = a + ((b-a)/phi)
-            else:
-                b = x2 
-                x2 = x1
-                x1 = b - ((b-a)/phi)
         
         
-        error = abs(b-a) # error is determined based on the absolute distance between to brackets as they repeatedly shirk over iterations
+        error = np.abs(b-a) # error is determined based on the absolute distance between to brackets as they repeatedly shirk over iterations
         
     return (a+b)/2, i # return midpoint between a and b, representing local extrema and how many iterations it took to reach the required tolerance
 
@@ -51,4 +56,3 @@ def newtonMinMaxMethod(x0, tol, f): #Newtons method for nonlinear optimization
         xprev = xnew # make the new xprev so operation could continue for k + 1 iters
         
     return xnew, i # condition is met, can return xnew as the local extrema and i as number of iterations it took to get there
-
