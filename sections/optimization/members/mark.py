d128623d92ed3c8a54f175b4aa315022339b315a
markp561 <235408474+markp561@users.noreply.github.com>
2025-12-02 19:14:04 -0500

    Update mark.py

--- sections/optimization/members/mark.py
+++ sections/optimization/members/mark.py
@@ -53,7 +53,7 @@ def newtonMinMaxMethod(x0, delta, sym_func):
     error = float('inf')
 
     sym_f_first = sp.diff(sym_func, x)
-    sym_f_second = sp.diff(sym_func_first, x)
+    sym_f_second = sp.diff(sym_f_first, x)
 
     f = sp.lambdify(x, sym_func, "numpy")
     f_first = sp.lambdify(x, sym_f_first, "numpy")
@@ -64,10 +64,9 @@ def newtonMinMaxMethod(x0, delta, sym_func):
 
     while error > delta and iterations < max_iter:
 
-        x_next = x0 - (alpha * (f_first.subs(x, x0) / f_second.subs(x, x0)))
+        x_next = x0 - (alpha * (f_first(x0) / f_second(x0)))
         error = np.abs(x_next - x0)
         x0 = x_next
         iterations += 1
 
     return x_next, iterations
-
