0d564c893b5cf8ea4f29b82655ced64b6607b135
Jhon Palaguachi <162334265+Jhon2468@users.noreply.github.com>
2025-12-03 22:49:42 -0500

    Refactor Newton's method to use sympy for derivatives

--- sections/root_finding/newton/jhon_newton.py
+++ sections/root_finding/newton/jhon_newton.py
@@ -1,18 +1,26 @@
+import sympy as sp 
+import math
+
 def newton_method(x0, tolerance, flag, user_function):
+    x = sp.Symbol('x')
+    
+    expression = sp.lambdify(x, user_function)
+    derivative_expression = sp.lambdify(x, sp.diff(user_function, x))
+    
     i = 0
     x1 = None
     estimated_root = None
     estimated_root_found = False
     
     while not(estimated_root_found) and i <= 1000:
-        if user_function(x0) == 0:
+        if expression(x0) == 0:
             estimated_root = x0
             estimated_root_found = True
         else:
-            if user_function(x0, 1) == 0:
+            if derivative_expression(x0) == 0:
                 x0 = float(input("Please enter a x0: "))
             else:
-                x1 = x0 - (user_function(x0) / user_function(x0, 1))
+                x1 = x0 - (expression(x0) / derivative_expression(x0))
                 
                 if flag == 1:
                     if abs(x1 - x0) < tolerance:
@@ -27,13 +35,13 @@ def newton_method(x0, tolerance, flag, user_function):
                     else:
                         x0 = x1
                 elif flag == 3:
-                    if (abs(user_function(x1)) < tolerance):
+                    if (abs(expression(x1)) < tolerance):
                         estimated_root = x1
                         estimated_root_found = True
                     else:
                         x0 = x1
                 else:
-                    if (abs(x1 - x0) < tolerance) and (abs(user_function(x1)) < tolerance):
+                    if (abs(x1 - x0) < tolerance) and (abs(expression(x1)) < tolerance):
                         estimated_root = x1
                         estimated_root_found = True
                     else:
