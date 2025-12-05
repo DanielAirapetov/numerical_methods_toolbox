ce7f96d976b557f3daee0c7a35c485afb190b00d
Jhon Palaguachi <162334265+Jhon2468@users.noreply.github.com>
2025-12-04 22:48:08 -0500

    Update about.py

--- pages/about.py
+++ pages/about.py
@@ -1,8 +1,1 @@
 import streamlit as st
-st.set_page_config(
-    page_title = "About",
-    initial_sidebar_state = "collapsed" 
-)
-
-if st.button("Back", use_container_width=True):
-    st.switch_page("app.py")
