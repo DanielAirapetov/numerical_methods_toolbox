cc7f2e22261e227eb576f960dfa1b315579360a7
Mark Pepaj <mpepaj1@gmail.com>
2025-12-05 18:30:14 -0500

    Started creating documentation page. Added title and horizontal line.

--- pages/documentation.py
+++ pages/documentation.py
@@ -1,1 +1,11 @@
 import streamlit as st
+
+st.set_page_config(layout = "wide")
+if st.button("Back"):
+    st.switch_page("app.py")
+
+st.markdown("<h1 style = 'text-align: center;'>Documentation</h1>", unsafe_allow_html = True)
+
+st.markdown("---")
+
+
