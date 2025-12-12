1f833103f1d8c39b369cfd421477f5d63e5213f6
Jhon Palaguachi <162334265+Jhon2468@users.noreply.github.com>
2025-12-12 01:28:21 -0500

    Update jhonS.py

--- sections/calculus/integration/simpson/jhonS.py
+++ sections/calculus/integration/simpson/jhonS.py
@@ -1,4 +1,3 @@
-import numpy as np
 def lagrange_function(z, x, f):
     n = len(x)
     interpolated_value = 0
@@ -12,8 +11,10 @@ def lagrange_function(z, x, f):
     
     return interpolated_value
 
-def simpson_rule(x, f, h):
-    n = len(x) - 1
+def simpson_rule(x_np, f_np, h):
+    x = list(x_np)
+    f = list(f_np)
+    n = int((x[-1] - x[0]) / h) 
     sum_01 = 0
     max_01 = round((((n / 2) / 10) + 1.0) - 0.1, 1) 
       
@@ -24,12 +25,12 @@ def simpson_rule(x, f, h):
         multiplier += 1
         even_data_point = round(data_point + (0.1 * multiplier), 1)
         if even_data_point in x:
-            sum_01 += f[np.where(x == even_data_point)[0][0]]
+            sum_01 += f[x.index(even_data_point)]
         else:
             left = round(even_data_point - 0.2, 1)
             right = round(even_data_point + 0.2, 1)
-            even_x_data_points = x[np.where(x == left)[0][0]:np.where(x == right)[0][0]]
-            even_x_values = f[np.where(x == left)[0][0]:np.where(x == right)[0][0]]
+            even_x_data_points = x[x.index(left):x.index(right)]
+            even_x_values = f[x.index(left):x.index(right)]
             sum_01 += lagrange_function(even_data_point, even_x_data_points, even_x_values)
         data_point = round(data_point + 0.1, 1)
     
@@ -43,12 +44,12 @@ def simpson_rule(x, f, h):
         multiplier += 1
         odd_data_point = round((data_point + (0.1 * multiplier)) - 0.1, 1)
         if odd_data_point in x:
-            sum_02 += f[np.where(x == odd_data_point)[0][0]]
+            sum_02 += f[x.index(odd_data_point)]
         else:
             left = round(odd_data_point - 0.2, 1)
             right = round(odd_data_point + 0.2, 1)
-            odd_x_data_points = x[np.where(x == left)[0][0]:np.where(x == right)[0][0]]
-            odd_x_values = f[np.where(x == left)[0][0]:np.where(x == right)[0][0]]
+            odd_x_data_points = x[x.index(left):x.index(right)]
+            odd_x_values = f[x.index(left):x.index(right)]
             sum_02 += lagrange_function(odd_data_point, odd_x_data_points, odd_x_values)
         data_point = round(data_point + 0.1, 1)
     
